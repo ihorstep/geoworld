3890f7b6bf836753180f71c4a3cdcca3a9cab194
Igor Stepakov <is@jelastic.com>
2020-05-17 21:27:02 +0300

    fix function

--- funcitons.php
+++ funcitons.php
@@ -1,14 +1,15 @@
 <?php
 
-$user = "root";
-$psd = "SITsdc32536";
+function connect() {
+    $user = "root";
+    $psd = "SITsdc32536";
 
-$dsn = "mysql:host=192.168.5.36;dbname=geoworld;port=3306;charset=utf8";
+    $dsn = "mysql:host=192.168.5.36;dbname=geoworld;port=3306;charset=utf8";
 
-$connection = new $PDO($dsn, $user, $psd);
+    $connection = new $PDO($dsn, $user, $psd);
 
-$connection->setAttribute(PDO::AFTER_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
+    $connection->setAttribute(PDO::AFTER_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
 
 function getContinents(PDO $connection) {
     $sql = "SELECT * FROM continents";
