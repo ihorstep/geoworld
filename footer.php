d02c744ef298c2a4211cc8b285d828835b6c23ad
Igor Stepakov <is@jelastic.com>
2020-05-18 23:26:38 +0300

    exclude header and footer to other files

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p class="bg-dark text-white text-center p-4">&copy; GeoWorld</p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 include("functions.php");
 
 $connection = connect();
-
 $continents = getContinents($connection);
-
 #print_r($continents);
 ?>
 
@@ -13,54 +11,28 @@ $continents = getContinents($connection);
     <head>
         <title>GeoWorld</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
     </head>
     <body>
-        <header>
-            <div class="container">
-              <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
-                    </li>                    
-                    <li class="nav-item">
-                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                  </ul>                 
-                </div>
-              </nav>
-              <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Library</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Data</li>
-              </ol>
-            </nav>
-            </div>            
-        </header>
+        <?= include("header.php"); ?>
         <main>
             <div class="container">
-                <?php foreach ($continents as $continentItem): ?>
-                <div class="col-6 col-md4" style="height: 100px;">
-                    <div class="card p-2 mb-2" style="width: 18rem;">
-                      <img src="images/continents/<?= strtolower($continentItem['code']) ?>.png" class="card-img-top" alt="<?= $continentItem['name']?>">
-                      <div class="card-body">
-                        <h5 class="card-title"><?= $continentItem['name']?></h5>
-                        <p class="card-text"><?= $continentItem['description'] ?></p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
+                <div class="row">
+                    <?php foreach ($continents as $continentItem): ?>
+                    <div class="col-6 col-md4">
+                        <div class="card p-2 mb-2">
+                          <img src="images/continents/<?= strtolower($continentItem['code']) ?>.png" class="card-img-top" alt="<?= $continentItem['name']?>">
+                          <div class="card-body">
+                            <h4 class="card-title">
+                                <a href="continent.php?code=<?= $continentItem['code']?>"></a>
+                                <?= $continentItem['name'] ?>
+                            </h4>
+                            <p class="card-text"><?= $continentItem['description'] ?></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
                     </div>
+                    <?php endforeach;?>
                 </div>
-                <?php endforeach;?>
                 <!--<div class="col-6 col-md4" style="height: 100px; border: 1px solid;"></div>
                 <div class="col-6 col-md4" style="height: 100px; border: 1px solid;"></div>
                 <div class="col-6 col-md4" style="height: 100px; border: 1px solid;"></div>
@@ -68,10 +40,7 @@ $continents = getContinents($connection);
                 <div class="col-6 col-md4" style="height: 100px; border: 1px solid;"></div>-->
             </div>
         </main>
-        <footer>
-            <div class="container">
-            </div>
-        </footer>
+        <?= include("footer.php"); ?>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
